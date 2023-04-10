ffa13429b7ae0db97c386b9033c2b644f067f47d
tahayasin <bastaha02@gmail.com>
2023-04-10 23:09:44 +0300

    Update untitled1.py

--- Proje/untitled1.py
+++ Proje/untitled1.py
@@ -1,4 +1,5 @@
 i=3
+x=1
 while(i!=0):
     giris=input("""Üyeliğiniz var mı? varsa "evet" yoksa "hayır" yazın""")
     if(giris=="evet"):
@@ -10,13 +11,14 @@ while(i!=0):
                 Kullanici_Adi=input("Kullanıcı adı belirleyiniz en fazla 9 karakter olabilir")
                 if(9<len(Kullanici_Adi)):
                     print("belirli olan karakter sayısından fazla karakter içeriyor tekrar deneyiniz")
+                    continue
                 else:
                     sifre=input("şifre belirleyiniz")
-                break
+                    break
         else:
             i=0
     print("Lobiye hoş geldiniz")
-    while(True):
+    while(x==1):
         Kullanici_Adi2=input("kullanıcı adınızı giriniz")
         if(Kullanici_Adi==Kullanici_Adi2):
             print("hoş geldiniz",Kullanici_Adi2)
@@ -24,6 +26,8 @@ while(i!=0):
             sifre2=input("şifrenizi giriniz")
             if(sifre==sifre2):
                 print("giriş başarılı")
+                x=2 
+                i=0
                 break
             else:
                 print("şifreniz hatalıdır bu kadar hakkınız kaldı",i)
@@ -32,12 +36,13 @@ while(i!=0):
                 if(i==0):
                     print("Tekrar başa döndürülüyosunuz")
                 if(numara=="1"):
-                    print("telefonuza mesaj gelecektir")
-                    break
+                    print("telefonuza şifre mesaj gelecektir")
+                    continue
                 else:
-                    break
+                    continue
         else:
             print("yanlış girdiniz tekrar deneyiniz")
             break
+          
           
 
